06a82736838267a2b22af015769d4e4fa9b983a4
Rossokhach_KSU <o.rossokhach@yandex.ru>
2021-02-19 17:27:35 +0300

    создан новый тест - удаление групп

---
+++ src/test/java/ru/stqa/pft/addressbook/GroupDeletionTest.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTest extends TestBase {
+
+  @Test
+  public void testGroupDelitions() throws Exception {
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
+
+  }
+
+}
+

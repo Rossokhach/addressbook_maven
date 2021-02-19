3e331e1a33c7a953bc8bc81d2dfeb3146ec81216
Rossokhach_KSU <o.rossokhach@yandex.ru>
2021-02-19 22:44:30 +0300

    создали 3 тест с применением уже имеющихся методов +2 новых
    создали 3 тест с применением уже имеющихся методов +2 новых

---
+++ src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTest extends TestBase{
+@Test
+  public void testGroupModification() {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test3", "test3", "test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+}
+}

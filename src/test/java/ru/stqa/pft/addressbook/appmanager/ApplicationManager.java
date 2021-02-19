29917c49fa1e72c1bd52c45cd84c1079302d8180
Rossokhach_KSU <o.rossokhach@yandex.ru>
2021-02-19 22:25:48 +0300

    внесены правки по уменьшению повторяющегося кода

--- src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,8 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -35,23 +33,7 @@ public class ApplicationManager {
         wd.quit();
     }
 
-    private boolean isElementPresent(By by) {
-      try {
-        wd.findElement(by);
-        return true;
-      } catch (NoSuchElementException e) {
-        return false;
-      }
-    }
 
-    private boolean isAlertPresent() {
-      try {
-        wd.switchTo().alert();
-        return true;
-      } catch (NoAlertPresentException e) {
-        return false;
-      }
-    }
 
     public GroupHelper getGroupHelper() {
         return groupHelper;
